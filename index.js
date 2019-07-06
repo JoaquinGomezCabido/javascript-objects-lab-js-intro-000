1c46f336990ff064dbdc6607a007eaa1577a8438
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-06 10:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
 }
 
 var deleteFromObjectByKey = (object, key) => {
+  var newObject = Object.assign ({}, object)
+  delete newObject.key
 
+  return newObject
 }
 
 var destructivelyDeleteFromObjectByKey = (object, key) => {
