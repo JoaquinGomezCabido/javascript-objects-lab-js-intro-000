4ee4ff52fb5de3a03d846f75eee429d2d16a4cb5
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-06 10:19:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var recipes = {}
 var updateObjectWithKeyAndValue = (object, key, value) => {
   return Object.assign ({}, object, [key], value)
 }
-/*
+
 var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
   object[key] = value
 
   return object
 }
-
+/*
 var deleteFromObjectByKey = (object, key) => {
 
 }
